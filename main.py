3f88535c0862e73859a8213de5c9f53da57488d1
jramilm <malatejramil@gmail.com>
2023-12-23 20:51:20 +0800

    add filter by type and id method and unit tests

--- main.py
+++ main.py
@@ -31,5 +31,37 @@ def get_pokemon():
     return make_response(jsonify(data), 200)
 
 
+@app.route("/api/pokemon/<int:pok_id>", methods=['GET'])
+def get_pokemon_by_id(pok_id):
+    data = data_fetch("SELECT * FROM pokemon WHERE pok_id = {}".format(pok_id))
+    return make_response(jsonify(data), 200)
+
+
+@app.route("/api/pokemon/<string:pok_name>", methods=['GET'])
+def get_pokemon_by_name(pok_name):
+    data = data_fetch("SELECT * FROM pokemon WHERE pok_name = '{}'".format(pok_name))
+    return make_response(jsonify(data), 200)
+
+
+@app.route("/api/pokemon/type", methods=['GET'])
+def get_all_types():
+    data = data_fetch("SELECT type_name FROM types")
+    return make_response(jsonify(data), 200)
+
+
+@app.route("/api/pokemon/type/<string:type_name>", methods=['GET'])
+def get_pokemon_by_type(type_name):
+    query = """
+           SELECT pt.pok_id, p.pok_name, t.type_name 
+           FROM pokemon_types pt
+           INNER JOIN pokemon p ON pt.pok_id = p.pok_id
+           INNER JOIN types t ON pt.type_id = t.type_id
+           WHERE t.type_name = '{}'
+       """.format(type_name)
+
+    data = data_fetch(query)
+    return make_response(jsonify(data), 200)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
--- unittests.py
+++ unittests.py
@@ -20,6 +20,21 @@ class TestPokemonApi(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTrue("bulbasaur" in response.data.decode())
 
+    def test_get_by_id(self):
+        response = self.app.get("/api/pokemon/1")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("bulbasaur" in response.data.decode())
+
+    def test_filter_name(self):
+        response = self.app.get("/api/pokemon/bulbasaur")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("bulbasaur" in response.data.decode())
+
+    def test_filter_by_type(self):
+        response = self.app.get("/api/pokemon/type/grass")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("grass" in response.data.decode())
+
 
 if __name__ == '__main__':
     unittest.main()
